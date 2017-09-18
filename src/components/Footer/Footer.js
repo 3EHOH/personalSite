7d6f0857ff410435786b287042eaffe9001158a5
3EHOH <zenonevans@gmail.com>
2017-09-18 14:04:35 -0500

    initial personalization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,23 +17,11 @@ class Footer extends React.Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
+          <span className={s.text}>Site owned by Zenon Evans</span>
           <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/">
-            Home
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/admin">
-            Admin
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">
-            Privacy
-          </Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">
-            Not Found
-          </Link>
+          <a href="https://www.reactstarterkit.com/">
+            Thanks to Kriasoft for boilerplate code
+          </a>
         </div>
       </div>
     );
